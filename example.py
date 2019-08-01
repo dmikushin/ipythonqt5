5d984359668164a6e8fa7c87e6cd0580034caee2
Dmitry Mikushin <dmitry@kernelgen.org>
2019-08-01 17:20:32 +0200

    Some refactorings

--- example.py
+++ example.py
@@ -6,15 +6,15 @@ sip.setapi("QString", 2)
 sip.setapi("QVariant", 2)
 from PyQt5.QtWidgets import *
 # Import the console machinery from ipython
-from qtconsole.rich_ipython_widget import RichIPythonWidget
+from qtconsole.rich_jupyter_widget import RichJupyterWidget
 from qtconsole.inprocess import QtInProcessKernelManager
 from IPython.lib import guisupport
 
-class QIPythonWidget(RichIPythonWidget):
+class QJupyterWidget(RichJupyterWidget):
     """ Convenience class for a live IPython console widget. We can replace the standard banner using the customBanner argument"""
     def __init__(self,customBanner=None,*args,**kwargs):
         if not customBanner is None: self.banner=customBanner
-        super(QIPythonWidget, self).__init__(*args,**kwargs)
+        super(QJupyterWidget, self).__init__(*args,**kwargs)
         self.kernel_manager = kernel_manager = QtInProcessKernelManager()
         kernel_manager.start_kernel()
         kernel_manager.kernel.gui = 'qt4'
@@ -47,12 +47,12 @@ class ExampleWidget(QWidget):
         super(ExampleWidget, self).__init__(parent)
         layout = QVBoxLayout(self)
         self.button = QPushButton('Another widget')
-        ipyConsole = QIPythonWidget(customBanner="Welcome to the embedded ipython console\n")
+        jupyterConsole = QJupyterWidget(customBanner="Welcome to the embedded Jupyter console\n")
         layout.addWidget(self.button)
-        layout.addWidget(ipyConsole)        
+        layout.addWidget(jupyterConsole)
         # This allows the variable foo and method print_process_id to be accessed from the ipython console
-        ipyConsole.pushVariables({"foo":43,"print_process_id":print_process_id})
-        ipyConsole.printText("The variable 'foo' and the method 'print_process_id()' are available. Use the 'whos' command for information.")                           
+        jupyterConsole.pushVariables({"foo":43,"print_process_id":print_process_id})
+        jupyterConsole.printText("The variable 'foo' and the method 'print_process_id()' are available. Use the 'whos' command for information.")
 
 def print_process_id():
     print("Process ID is: %d" % os.getpid())
